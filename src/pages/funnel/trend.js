596e44dcd588b878120e7370dfc095413dc28bf6
Woohyun Kim <woohyun.kim@hakko-dev.com>
2020-02-27 10:17:13 +0900

    퍼널 트렌드

--- src/pages/funnel/trend.js
+++ src/pages/funnel/trend.js
@@ -1,134 +1,37 @@
-var ctx = document.getElementById('trend').getContext('2d');
-
-var lineChartData = {
-  labels: ['1', '2', '3', '4', '5', '6', '7'],
-  datasets: [
-    {
-      label: '방문객',
+window.chart1 = Chart.Bar(document.getElementById('chart1').getContext('2d'), {
+  data: {
+    labels: _.range(1,32),
+    datasets: [{
+      label: '통행객',
       borderColor: 'rgb(0, 138, 255)',
       backgroundColor: 'rgb(0, 138, 255)',
       fill: false,
-      data: [
-        randomScalingFactor(0, 30000),
-        randomScalingFactor(0, 30000),
-        randomScalingFactor(0, 30000),
-        randomScalingFactor(0, 30000),
-        randomScalingFactor(0, 30000),
-        randomScalingFactor(0, 30000),
-        randomScalingFactor(0, 30000)
-      ],
-      yAxisID: 'y-axis-1'
-    },
-    {
-      label: '체류객',
-      borderColor: 'rgb(79, 91, 255)',
-      backgroundColor: 'rgb(79, 91, 255)',
-      fill: false,
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ],
-      yAxisID: 'y-axis-1'
-    },
-    {
-      label: '매출액',
-      borderColor: 'rgb(39, 53, 140)',
-      backgroundColor: 'rgb(39, 53, 140)',
-      fill: false,
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ],
-      yAxisID: 'y-axis-1'
-    },
-    {
-      label: '방문율',
-      borderColor: 'rgb(0, 234, 66)',
-      backgroundColor: 'rgb(0, 234, 66)',
-      borderDash: [5, 5],
-      fill: false,
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ],
-      yAxisID: 'y-axis-2'
-    },
-    {
-      label: '체류율',
-      borderColor: 'rgb(190, 219, 0)',
-      backgroundColor: 'rgb(190, 219, 0)',
-      borderDash: [5, 5],
-      fill: false,
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ],
-      yAxisID: 'y-axis-2'
-    },
-    {
-      label: '구매율',
-      borderDash: [5, 5],
-      borderColor: 'rgb(255, 180, 40)',
-      backgroundColor: 'rgb(255, 180, 40)',
-      fill: false,
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ],
-      yAxisID: 'y-axis-2'
-    }
-  ]
-};
-
-window.trendChart = Chart.Line(ctx, {
-  data: lineChartData,
+      data: _.range(1,32).map(function(i){return randomScalingFactor(0,30000)})
+    }]
+  },
   options: {
     responsive: true,
     maintainAspectRatio: false,
     hoverMode: 'index',
     stacked: false,
     title: {
-      display: false
+      display: false,
     },
-    legend: {
+    legend:{
+      display: false,
       position: 'bottom',
       labels: {
         boxWidth: 12
       }
     },
-    elements: {
+    elements:{
       line: {
-        borderWidth: 2,
-        cubicInterpolationMode: 'default',
-        tension: 0
+        borderWidth:2,
+        cubicInterpolationMode:'default',
+        tension: 0,
       },
       point: {
-        radius: 1
+        radius: 4
       }
     },
     tooltips: {
@@ -136,71 +39,397 @@ window.trendChart = Chart.Line(ctx, {
       intersect: false
     },
     scales: {
-      gridLines: {
+      gridLines:{
         display: false,
-        ticks: {}
-      },
-      xAxes: [
-        {
-          gridLines: {
-            display: false
-          }
+        ticks: {
+
         }
-      ],
-      yAxes: [
-        {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          id: 'y-axis-1',
-          ticks: {
-            padding: 10,
-            callback: function(value) {
-              if (value >= 10000) {
-                return value / 10000 + '만';
-              }
-              return value;
+      },
+      xAxes : [ {
+        gridLines : {
+          display : false
+        }
+      }],
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        ticks: {
+          padding: 10,
+          callback: function(value){
+            if(value >= 10000){
+              return value / 10000 + "만"
             }
-          },
-          gridLines: {
-            drawBorder: false
+            return value
           }
         },
-        {
-          type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-          display: true,
-          position: 'right',
-          id: 'y-axis-2',
-          ticks: {
-            padding: 10,
-            callback: function(value) {
-              return value + '%';
-            }
-          },
-          // grid line settings
-          gridLines: {
-            drawTicks: false,
-            drawBorder: false,
-            display: false,
-            drawOnChartArea: false // only want the grid lines for one axis to show up
-          }
+        gridLines: {
+          drawBorder: false
         }
-      ]
+      }],
     }
   }
 });
 
-/**
- * day, week, month
- */
-window.setTrendTimeRange = function(range) {
-  // 업데이트
-  // lineChartData.labels = [] // 맨 아래 일, 주, 월 항목들
-  lineChartData.datasets.forEach(function(dataset) {
-    dataset.data = dataset.data.map(function() {
-      return randomScalingFactor(); // 실제 값 넣기
-    });
-  });
+window.chart2 = Chart.Bar(document.getElementById('chart2').getContext('2d'), {
+  data: {
+    labels: _.range(1,32),
+    datasets: [{
+      label: '방문객',
+      borderColor: 'rgb(0, 138, 255)',
+      backgroundColor: 'rgb(0, 138, 255)',
+      fill: false,
+      data: _.range(1,32).map(function(i){return randomScalingFactor(0,30000)})
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    hoverMode: 'index',
+    stacked: false,
+    title: {
+      display: false,
+    },
+    legend:{
+      display: false,
+      position: 'bottom',
+      labels: {
+        boxWidth: 12
+      }
+    },
+    elements:{
+      line: {
+        borderWidth:2,
+        cubicInterpolationMode:'default',
+        tension: 0,
+      },
+      point: {
+        radius: 4
+      }
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    scales: {
+      gridLines:{
+        display: false,
+        ticks: {
 
-  window.trendChart.update();
-};
+        }
+      },
+      xAxes : [ {
+        gridLines : {
+          display : false
+        }
+      }],
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        ticks: {
+          padding: 10,
+          callback: function(value){
+            if(value >= 10000){
+              return value / 10000 + "만"
+            }
+            return value
+          }
+        },
+        gridLines: {
+          drawBorder: false
+        }
+      }],
+    }
+  }
+});
+
+window.chart3 = Chart.Bar(document.getElementById('chart3').getContext('2d'), {
+  data: {
+    labels: _.range(1,32),
+    datasets: [{
+      label: '체류객',
+      borderColor: 'rgb(0, 138, 255)',
+      backgroundColor: 'rgb(0, 138, 255)',
+      fill: false,
+      data: _.range(1,32).map(function(i){return randomScalingFactor(0,2000)})
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    hoverMode: 'index',
+    stacked: false,
+    title: {
+      display: false,
+    },
+    legend:{
+      display: false,
+      position: 'bottom',
+      labels: {
+        boxWidth: 12
+      }
+    },
+    elements:{
+      line: {
+        borderWidth:2,
+        cubicInterpolationMode:'default',
+        tension: 0,
+      },
+      point: {
+        radius: 4
+      }
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    scales: {
+      gridLines:{
+        display: false,
+        ticks: {
+
+        }
+      },
+      xAxes : [ {
+        gridLines : {
+          display : false
+        }
+      }],
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        ticks: {
+          padding: 10,
+          callback: function(value){
+            if(value >= 10000){
+              return value / 10000 + "만"
+            }
+            return value
+          }
+        },
+        gridLines: {
+          drawBorder: false
+        }
+      }],
+    }
+  }
+});
+
+window.chart4 = Chart.Line(document.getElementById('chart4').getContext('2d'), {
+  data: {
+    labels: _.range(1,32),
+    datasets: [{
+      label: '방문율',
+      borderColor: 'rgb(0, 138, 255)',
+      backgroundColor: 'rgb(0, 138, 255)',
+      fill: false,
+      data: _.range(1,32).map(function(i){return randomScalingFactor(0,4)})
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    hoverMode: 'index',
+    stacked: false,
+    title: {
+      display: false,
+    },
+    legend:{
+      display: false,
+      position: 'bottom',
+      labels: {
+        boxWidth: 12
+      }
+    },
+    elements:{
+      line: {
+        borderWidth:2,
+        cubicInterpolationMode:'default',
+        tension: 0,
+      },
+      point: {
+        radius: 4
+      }
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    scales: {
+      gridLines:{
+        display: false,
+        ticks: {
+
+        }
+      },
+      xAxes : [ {
+        gridLines : {
+          display : false
+        }
+      }],
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        ticks: {
+          padding: 10,
+          callback: function(value){
+            return value + "%"
+          }
+        },
+        gridLines: {
+          drawBorder: false
+        }
+      }],
+    }
+  }
+});
+
+window.chart5 = Chart.Line(document.getElementById('chart5').getContext('2d'), {
+  data: {
+    labels: _.range(1,32),
+    datasets: [{
+      label: '체류율',
+      borderColor: 'rgb(0, 138, 255)',
+      backgroundColor: 'rgb(0, 138, 255)',
+      fill: false,
+      data: _.range(1,32).map(function(i){return randomScalingFactor(60,70)})
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    hoverMode: 'index',
+    stacked: false,
+    title: {
+      display: false,
+    },
+    legend:{
+      display: false,
+      position: 'bottom',
+      labels: {
+        boxWidth: 12
+      }
+    },
+    elements:{
+      line: {
+        borderWidth:2,
+        cubicInterpolationMode:'default',
+        tension: 0,
+      },
+      point: {
+        radius: 4
+      }
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    scales: {
+      gridLines:{
+        display: false,
+        ticks: {
+
+        }
+      },
+      xAxes : [ {
+        gridLines : {
+          display : false
+        }
+      }],
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        ticks: {
+          padding: 10,
+          callback: function(value){
+            return value + "%"
+          },
+          beginAtZero: true,
+          suggestedMax: 100
+        },
+        gridLines: {
+          drawBorder: false
+        }
+      }],
+    }
+  }
+});
+
+window.chart6 = Chart.Bar(document.getElementById('chart6').getContext('2d'), {
+  data: {
+    labels: _.range(1,32),
+    datasets: [{
+      label: '체류시간',
+      borderColor: 'rgb(0, 138, 255)',
+      backgroundColor: 'rgb(0, 138, 255)',
+      fill: false,
+      data: _.range(1,32).map(function(i){return randomScalingFactor(0,18)})
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    hoverMode: 'index',
+    stacked: false,
+    title: {
+      display: false,
+    },
+    legend:{
+      position: 'bottom',
+      labels: {
+        boxWidth: 12
+      }
+    },
+    elements:{
+      line: {
+        borderWidth:2,
+        cubicInterpolationMode:'default',
+        tension: 0,
+      },
+      point: {
+        radius: 4
+      }
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    scales: {
+      gridLines:{
+        display: false,
+        ticks: {
+
+        }
+      },
+      xAxes : [ {
+        gridLines : {
+          display : false
+        }
+      }],
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        ticks: {
+          padding: 10,
+          callback: function(value){
+            return value + "분"
+          }
+        },
+        gridLines: {
+          drawBorder: false
+        }
+      }],
+    }
+  }
+});
